f0f76f29137fe2d0d11ff8e6fa0307c8f6e3c9a8
KrisZbs <80694226+KrisZbs@users.noreply.github.com>
2021-03-21 23:15:05 +0700

    Add files via upload
    Не получается распечатать двумерный массив из 4 задания. И еще не видит [j].

---
+++ Task1.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 1, 0, 0, 1, 0, 1, 1, 0, 1};
+        int i;
+        for (i = 0; i < arr.length; i++) {
+            if (arr[i] == 1) {
+                arr[i] = 0;
+            } else {
+                arr[i] = 1;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+
+}
